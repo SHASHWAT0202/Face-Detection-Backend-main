1a46086fc75f966835207c741f5f6cf2a491a0f8
SHASHWAT0202 <focusedshashwat@gmail.com>
2025-12-15 01:53:39 +0530

    fourth-commit

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5500",
+    "http://localhost:8080",
     "http://localhost:5500",
     "https://shashwat0202.github.io",
 ]
